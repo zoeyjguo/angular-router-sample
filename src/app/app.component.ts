0d0b90c664fa544c78c58162265585efa8398b8a
Zoey Guo <zoey-guo@uiowa.edu>
2024-07-08 15:02:30 -0500

    finished tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CrisisListComponent, HeroesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
